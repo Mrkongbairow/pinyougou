a44ea5207421741d005d8b9a56ca6e659905f31a
zhangsan <1197526628@qq.com>
2019-07-15 18:49:22 +0800

    CAS 单点登录功能完成

---
+++ itheima-sms-service/src/main/java/com/sms/AliyunSmsDemo.java
@@ -0,0 +1,13 @@
+package com.sms;
+
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.http.MethodType;
+import com.aliyuncs.profile.DefaultProfile;
+
+public class AliyunSmsDemo {
+    public static void main(String[] args) {
+
+    }
+}
